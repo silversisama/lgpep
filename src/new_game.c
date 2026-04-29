41a1ecb66d5936449f896746a96c850c4e5ed61c
silversisama <jaysonsheets.writer@gmail.com>
2026-04-29 13:44:54 -0500

    missed a setting for autorun feature

--- src/new_game.c
+++ src/new_game.c
@@ -109,6 +109,7 @@ static void SetDefaultOptions(void)
     gSaveBlock2Ptr->optionsBattleStyle = OPTIONS_BATTLE_STYLE_SHIFT;
     gSaveBlock2Ptr->optionsBattleSceneOff = FALSE;
     gSaveBlock2Ptr->regionMapZoom = FALSE;
+    gSaveBlock2Ptr->optionsAutoRun = FALSE;  // Default to OFF (classic behavior)
 }
 
 static void ClearPokedexFlags(void)
